2f6e5f7afbc0fda1684b3d06aea201233006e1ec
aashish24192 <aashish24192@gmail.com>
2024-01-08 20:57:41 +0530

    Latest Commit_08Jan

---
+++ src/basics_01/Lab002.java
@@ -0,0 +1,19 @@
+package src.basics_01;
+
+public class Lab002 {
+
+    public static void main(String[] args) {
+        byte age = 10;
+
+        age = 127;
+
+        System.out.println(age+1);
+
+        System.out.println(age);
+
+        short _age=128;
+        System.out.println(_age);
+
+
+    }
+}
---
+++ src/basics_02/Lab003.java
@@ -0,0 +1,9 @@
+package src.basics_02;
+
+public class Lab003 {
+
+    public static void main(String[] args) {
+
+        int age =10;
+    }
+}
